693c4902128d969548e6a0db749b6fa516d38eb8
Muhammad Farabie <farabie024@gmail.com>
2024-08-04 14:57:40 +0700

    String

---
+++ string/escape_sequence.js
@@ -0,0 +1,20 @@
+/* 
+    Javascript mendukung escape sequence di string
+    Escape Sequence merupakan karakter khsusu seperti tap dan lain-lain
+
+    \n -> Enter
+    \t -> Tab
+    \' -> '
+    \" -> "
+    \\ -> \
+*/
+
+// document.writeln("<textarea cols ='100' rows='10'");
+document.writeln("Muhammad \nFarabie \nBelajar Javascript");
+document.writeln("</br>");
+document.writeln("\\Home \\Farabie");
+document.writeln("</br>");
+document.writeln("\"Belajar Javascript 2024\"");
+document.writeln("</br>");
+document.writeln("\'Javascript Belajar\'");
+// document.writeln("<textarea>");
---
+++ string/string.js
@@ -0,0 +1,8 @@
+/* 
+    Tipe data string adalah tipe data yang berisi kumpulan kosong atau lebih karakter
+    Di javascript kita bisa dapat menggunakan petik dua("") atau petik 1 ('')
+*/
+
+document.writeln("Muhammad Farabie");
+document.writeln("</br>");
+document.writeln("Belajar Javascript 2024");
